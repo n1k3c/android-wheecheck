bca072b2cfbbb2ee4c3e7d2ed1fddcf4caf56d6d
Nikola Curilovic <ncurilovic@gmail.com>
2014-10-21 18:38:50 +0200

    Presence work

--- app/src/main/java/com/wheelabs/wheecheck/wheecheck/FragmentUserHomeContent.java
+++ app/src/main/java/com/wheelabs/wheecheck/wheecheck/FragmentUserHomeContent.java
@@ -1,6 +1,8 @@
 package com.wheelabs.wheecheck.wheecheck;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +10,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 /**
  * Created by nikola on 10/16/14.
@@ -16,6 +29,7 @@ public class FragmentUserHomeContent extends Fragment implements View.OnClickLis
 
     Button bScan;
     TextView tvResult;
+    String result;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -33,8 +47,80 @@ public class FragmentUserHomeContent extends Fragment implements View.OnClickLis
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String result = data.getStringExtra("result");
-        tvResult.setText("Your classrom: " + result);
+        result = data.getStringExtra("result");
+        if (result != null){
+            tvResult.setText("Your classrom: " + result);
+            new SendResultToParse().execute(result);
+        }else {
+            Toast.makeText(getActivity(), "Fail! Please scan QR code again.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    class SendResultToParse extends AsyncTask<String, Integer, String>{
+
+        ProgressDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = new ProgressDialog(getActivity());
+            dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            dialog.setTitle("Loading...");
+            dialog.setMessage("Please wait.");
+            dialog.show();
+        }
+
+
+        @Override
+        protected String doInBackground(String... arg0) {
+
+            // Get parameter from execute()
+            final String classroom = arg0[0];
+
+            // Get Username
+            ParseUser currentUser = ParseUser.getCurrentUser();
+            final String username = (String) currentUser.get("username");
+            final boolean presence = true;
+
+            final ParseQuery<ParseObject> query = ParseQuery.getQuery("UserActivity");
+            query.whereEqualTo("username", username);
+            query.whereEqualTo("classroom", classroom);
+            query.getFirstInBackground(new GetCallback<ParseObject>() {
+                @Override
+                public void done(ParseObject parseObject, ParseException e) {
+                    if (parseObject == null){
+                        ParseObject userActivity = new ParseObject("UserActivity");
+                        userActivity.put("username", username);
+                        userActivity.put("classroom", classroom);
+                        userActivity.put("presence", presence);
+                        userActivity.saveInBackground();
+                    }else {
+                        boolean presenceNew = parseObject.getBoolean("presence");
+                        if(presenceNew == false){
+                            presenceNew = true;
+                            parseObject.put("presence", presenceNew);
+                            parseObject.saveInBackground();
+                        }else {
+                            presenceNew = false;
+                            parseObject.put("presence", presenceNew);
+                            parseObject.saveInBackground();
+                        }
+
+                    }
+                }
+            });
+
+
+
+            dialog.dismiss();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
     }
 
 }
